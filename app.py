999baea7e76fbbf6583bb2f3a6754e00bc108f57
prudvireddyNS <22102A030231@mbu.asia>
2024-07-29 21:44:49 +0530

    service account

--- app.py
+++ app.py
@@ -12,14 +12,14 @@ import json
 import os
 from pprint import pprint
 
-private_key_id = os.getenv('PRIVATE_KEY_ID')
-private_key = os.getenv('PRIVATE_KEY')
-client_email = os.getenv('CLIENT_EMAIL')
-client_id = os.getenv('CLIENT_ID')
-auth_uri = os.getenv('AUTH_URI')
-token_uri = os.getenv('TOKEN_URI')
-auth_provider_x509_cert_url = os.getenv('AUTH_PROVIDER_X509_CERT_URL')
-client_x509_cert_url = os.getenv('CLIENT_X509_CERT_URL')
+private_key_id = os.getenv('private_key_id')
+private_key = os.getenv('private_key')
+client_email = os.getenv('client_email')
+client_id = os.getenv('client_id')
+auth_uri = os.getenv('auth_uri')
+token_uri = os.getenv('token_uri')
+auth_provider_x509_cert_url = os.getenv('auth_provider_x509_cert_url')
+client_x509_cert_url = os.getenv('client_x509_cert_url')
 universe_domain =  os.getenv("universe_domain")
 
 service_account_info = {
